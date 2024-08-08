af691e549f77e1bf9dbe40c4d16b9839becbdaf6
Jairo Lozano <jairo25492@gmail.com>
2024-08-08 08:45:08 -0600

    002 Crea POST handler sin arquitectura hexagonal

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,15 +1,58 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ContentItemCreateParams is the struct that represents the request body for creating a content item
+type ContentItemCreateParams struct {
+	Category    string `json:"category"`
+	Description string `json:"description"`
+	Title       string `json:"title"`
+}
+
+// ContentItem reflects a content item in the system.
+type ContentItem struct {
+	Category              string     `json:"category"`
+	CreatedAt             *time.Time `json:"created_at"`
+	Description           string     `json:"description"`
+	PublishedAtBlog       *time.Time `json:"published_at_blog"`
+	PublishedAtLinkedIn   *time.Time `json:"published_at_linkedin"`
+	PublishedAtNewsletter *time.Time `json:"published_at_newsletter"`
+	PublishedAtYoutube    *time.Time `json:"published_at_youtube"`
+	Title                 string     `json:"title"`
+	UpdatedAt             *time.Time `json:"updated_at"`
+	UserID                string     `json:"-"`
+}
 
 func main() {
 	r := gin.Default()
 
-	r.POST("/content", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello go!",
-		})
-	})
+	r.POST("/content", Get)
 
 	r.Run() // listen and serve on 0.0.0.0:8080 by default
+}
+
+func Get(c *gin.Context) {
+	var contentItemCreateParams ContentItemCreateParams
+	if err := c.ShouldBindJSON(&contentItemCreateParams); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	now := time.Now().UTC()
+	contentItem := ContentItem{
+		Category:    contentItemCreateParams.Category,
+		Description: contentItemCreateParams.Description,
+		Title:       contentItemCreateParams.Title,
+		CreatedAt:   &now,
+		UpdatedAt:   &now,
+	}
+
+	// Todo: Save contentItem to database
+
+	c.JSON(200, contentItem)
 }
