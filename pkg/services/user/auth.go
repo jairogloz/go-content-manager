47b930ac4306a24cbcdcad684e472f1672dc2100
Jairo Lozano <jairo25492@gmail.com>
2024-08-17 13:46:31 -0600

    030 Implementa hashing en Auth service

--- pkg/services/user/auth.go
+++ pkg/services/user/auth.go
@@ -1,12 +1,36 @@
 package user
 
-import "github.com/jairogloz/go-content-manager/pkg/domain"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+
+	"github.com/jairogloz/go-content-manager/pkg/domain"
+)
 
 func (s *Service) Auth(apiKey string) (*domain.User, error) {
-	user, err := s.Repo.FindByAPIKey(apiKey)
+
+	hashedKey := hashText(apiKey)
+
+	user, err := s.Repo.FindByAPIKey(hashedKey)
 	if err != nil {
 		return nil, err
 	}
 
 	return user, nil
+}
+
+func hashText(text string) string {
+	// Crear un nuevo hasher SHA-256
+	hasher := sha256.New()
+
+	// Escribir el texto en el hasher
+	hasher.Write([]byte(text))
+
+	// Obtener el hash en formato byte slice
+	hashBytes := hasher.Sum(nil)
+
+	// Convertir el hash a una cadena hexadecimal
+	hashString := hex.EncodeToString(hashBytes)
+
+	return hashString
 }
